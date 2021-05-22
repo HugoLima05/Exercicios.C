f3e3ded799e3b0ee28b4d1ce284575a04c68cd42
Hugo Lima <84336049+HugoLima05@users.noreply.github.com>
2021-05-22 15:19:55 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int dia,mes,ano;
+    printf("Ingrese el dia:");
+    scanf("%d",&dia);
+    printf("Ingrese el mes:");
+    scanf("%d",&mes);
+    printf("Ingrese el ano:");
+    scanf("%d",&ano);
+    printf("%d/%d/%d",dia,mes,ano);
+    return 0;
+}
